0a115936eda5122189db4e98b4874ffec5bbc6db
jason34105533 <a20040108jason@gmail.com>
2025-08-22 21:51:21 +0800

    [Update] Better securtiy, data log.

--- Curl_GoShare.py
+++ Curl_GoShare.py
@@ -2,20 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 from email.mime.text import MIMEText
+import json
+import os
 
+# Curl Usage
 HOST_LINK = "https://www.ptt.cc/bbs"
-SECTION = "Lifeismoney"
-QUEST = "goshare"
+SECTION = os.environ.get("SECTION")
+QUEST = os.environ.get("QUEST")
 
-ATTACH_LINK = "https://go.ridegoshare.com"
+ATTACH_LINK = os.environ.get("ATTACH_LINK")
 
+# Personal Info
+MY_MAIL = os.environ.get("MY_MAIL")
+SECURTIYCODE = os.environ.get("SECURTIYCODE")
+
+# Options
 DO_SEARCH = True
 DO_ATTACH = True
 
-
 SEARCH_PAGE = (HOST_LINK + "/" + SECTION + "/search?q=" + QUEST) if (DO_SEARCH == True) else (HOST_LINK + "/" + SECTION + "/index.html")
 
-
+# Function Implementation
 def Retreive_Text(link: str) -> str:
     response = requests.get(link)
 
@@ -54,7 +61,7 @@ def Curl_LinkAndTitle(SEARCH_PAGE :str) -> tuple :
                 article_link.append("Article Have been removed")
                 article_title.append("Article Have been removed")
                 
-    return article_link, article_title
+    return article_title, article_link
 
 def Mail_It(Title: str ,Content: str) -> set :
     
@@ -65,14 +72,14 @@ def Mail_It(Title: str ,Content: str) -> set :
     # msg['Subject'] = '快搶！ 有新的GoShare優惠卷！'            # 郵件標題
     msg['Subject'] = Title            # 郵件標題
     msg['From'] = 'GoShare Beggar'                  # 暱稱或是 email
-    msg['To'] = 'a20040108jason@gmail.com'   # 收件人 email
-    # msg['Cc'] = 'a20040108jason@gmail.com'   # 副本收件人 email ( 開頭的 C 大寫 )
-    # msg['Bcc'] = 'a20040108jason@gmail.com'  # 密件副本收件人 email
+    msg['To'] = MY_MAIL   # 收件人 email
+    # msg['Cc'] = MY_MAIL   # 副本收件人 email ( 開頭的 C 大寫 )
+    # msg['Bcc'] = MY_MAIL  # 密件副本收件人 email
 
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('a20040108jason@gmail.com','xnhedhktysiasrne')
+    smtp.login(MY_MAIL,SECURTIYCODE)
     status = smtp.send_message(msg)    # 改成 send_message
     if status == {}:
         print('郵件傳送成功！')
@@ -80,7 +87,8 @@ def Mail_It(Title: str ,Content: str) -> set :
         print('郵件傳送失敗！')
     smtp.quit()
 
-def CheckNewPost(Titles :list, Links: list, Link_Set: set) :
+
+def Check_NewPost(Titles :list, Links: list, Link_Set: set) :
     for (title, link) in zip(Titles, Links):
         if(link not in Link_Set):
             tmp_Content = Retreive_Text(link)
@@ -89,17 +97,44 @@ def CheckNewPost(Titles :list, Links: list, Link_Set: set) :
     
     return Link_Set
 
-Links, Titles = Curl_LinkAndTitle(SEARCH_PAGE)
+def Flush_Back(Titles :list, Links: list) :
+    Dic = dict()
+    Dic["updated_at"] = "2025-08-22"
+    
+    Data = []
+    for (title, link) in zip(Titles, Links):
+        Data.append({"Title" : title, "Link" : link})
+        
+    Dic["data"] = Data
+    
+    jsonFile = open('./.store/history.json','w', encoding="utf-8")
+    json.dump(Dic, jsonFile, indent=4, ensure_ascii=False)
+    
+    
+def INIT():
+    if(os.path.exists(".store") == False):
+        directory_name = ".store"
+        os.mkdir(directory_name)
+    
+    
+        
 
-Content_of_1 = Retreive_Text(Links[0])
-Title_of_1 = Titles[0]
+if __name__ == "__main__":
 
-print([Title_of_1, Content_of_1])
+    INIT()
+    
+    Titles, Links = Curl_LinkAndTitle(SEARCH_PAGE)
 
-Link_Set = set({'https://www.ptt.cc/bbs/Lifeismoney/M.1754649135.A.018.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1750074707.A.EC4.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752833242.A.509.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752030784.A.B2F.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751022759.A.72F.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1753438483.A.1D1.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752240299.A.6E5.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1754039089.A.D23.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752980809.A.486.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752624603.A.4B9.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751900624.A.530.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751627929.A.43B.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1755252333.A.475.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752390321.A.DAE.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1754205551.A.400.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751548003.A.5FF.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751282995.A.DDC.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751469142.A.B51.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752834550.A.AC6.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1750414475.A.E9C.html'})
-# Link_Set = CheckNewPost(Titles, Links, Link_Set)
+    Content_of_1 = Retreive_Text(Links[0])
+    Title_of_1 = Titles[0]
 
-print(Link_Set)
+    # print([Title_of_1, Content_of_1])
 
-Mail_It(Title_of_1, Content_of_1)
+    Link_Set = set({'https://www.ptt.cc/bbs/Lifeismoney/M.1754649135.A.018.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1750074707.A.EC4.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752833242.A.509.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752030784.A.B2F.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751022759.A.72F.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1753438483.A.1D1.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752240299.A.6E5.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1754039089.A.D23.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752980809.A.486.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752624603.A.4B9.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751900624.A.530.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751627929.A.43B.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1755252333.A.475.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752390321.A.DAE.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1754205551.A.400.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751548003.A.5FF.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751282995.A.DDC.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1751469142.A.B51.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1752834550.A.AC6.html', 'https://www.ptt.cc/bbs/Lifeismoney/M.1750414475.A.E9C.html'})
+    Link_Set = Check_NewPost(Titles, Links, Link_Set)
 
+    print(Link_Set)
+
+    # Mail_It(Title_of_1, Content_of_1)
+
+    Flush_Back(Titles, Links)
